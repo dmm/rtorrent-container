c367c7bd3e81b0b7312a6995d4b96ee28c0d494e
David Matthew Mattli <dmm@framey.lan>
2024-07-15 11:42:23 -0500

    fix: set fail on error in start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
+
+set -euo pipefail
+
+if [ ! -f /rtorrent.rc ]; then
+    echo "rtorrent.rc not found!"
+    exit 1
+fi
+
 tmux new-session -d -s rtorrent 'rtorrent -D -I -o import=/rtorrent.rc'
 sleep infinity
